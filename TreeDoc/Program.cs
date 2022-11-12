d65ed5a6c29b532bc6d2a3e0447e1553f16a5ed7
Adrian Sims <adriansims62781@gmail.com>
2022-11-12 05:54:17 +0100

    first commit

---
+++ TreeDoc/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TreeDoc;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Hello");
+        }
+        catch (Exception ex)
+        {
+            var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+            var progname = Path.GetFileNameWithoutExtension(fullname);
+            Console.Error.WriteLine($"{progname}  Error: {ex}");
+        }
+    }
+}
+
