20b271ffeb709feed7e54531d7b03754882a2cb5
Adrian Sims <adriansims62781@gmail.com>
2022-11-15 11:19:44 +0100

    updates

--- TreeDoc/Program.cs
+++ TreeDoc/Program.cs
@@ -1,11 +1,28 @@
-﻿namespace TreeDoc;
+﻿using System.Xml;
+
+namespace TreeDoc;
+
 class Program
 {
     private static void Main(string[] args)
     {
         try
         {
-            Console.WriteLine("Hello");
+            var doc = TDoc.Parse(TestDoc);
+            var xdoc = doc.Content;
+
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                Indent = true,
+                OmitXmlDeclaration = true
+            };
+            using (XmlWriter writer = XmlWriter.Create(Console.Out, settings))
+            {
+                xdoc.WriteTo(writer);
+            }
+
+            Console.WriteLine("done");
+
         }
         catch (Exception ex)
         {
@@ -14,5 +31,15 @@ class Program
             Console.Error.WriteLine($"{progname}  Error: {ex}");
         }
     }
+
+    private const string TestDoc = """
+Hello said the man
+= This is level 0
+This is some text in level 0
+== This is level 1
+This is some level 1 text
+== This is also level 1
+This is some level 1 text as well
+""";
 }
 
---
+++ TreeDoc/TDoc.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace TreeDoc;
+
+class TDoc
+{
+    private XDocument _content;
+    private TDoc(XDocument content)
+    {
+        _content = content;
+    }
+
+    public static TDoc Parse(string content)
+    {
+        using var reader = new StringReader(content);
+
+        string? line;
+        int lineNumber = 1;
+        int currentLevel = 0;
+        StringBuilder currentContent = new();
+        var xdoc = new XDocument();
+        xdoc.Add(new XElement("Document"));
+        var currentElement = xdoc.Root;
+        
+        while ((line = reader.ReadLine()) != null)
+        {
+            var nextLevel = 0;
+            int i = 0;
+            while (i < line.Length)
+            {
+                if (line[i++] == '=')
+                {
+                    nextLevel++;
+                }
+            }
+
+            if (nextLevel == 0)
+            {
+                currentContent.AppendLine(line);
+            }
+            else if (nextLevel < currentLevel)
+            {
+
+            }
+            else if (nextLevel == currentLevel)
+            {
+                currentElement.Add(currentContent.ToString().Trim());
+                var elementName = $"L{currentLevel - 1}";
+                var element = new XElement(elementName);
+                currentElement?.AddAfterSelf(element);
+                currentElement = element;
+                currentContent.Clear();
+            }
+            else if (nextLevel == currentLevel + 1)
+            {
+                var elementName = $"L{currentLevel}";
+                var element = new XElement(elementName);
+                currentElement.Add(element);
+                currentElement.Add(currentContent.ToString().Trim());
+                currentElement = element;
+                currentContent.Clear();
+                currentLevel = nextLevel;
+            }
+            else if (nextLevel > currentLevel)
+            {
+                throw new Exception($"bad level at line {lineNumber}");
+            }
+            lineNumber++;
+        }
+        if (currentContent.Length > 0)
+        {
+            currentElement?.Add(currentContent.ToString().Trim());
+        }
+
+        var doc = new TDoc(xdoc);
+        return doc;
+    }
+
+    public XDocument Content => _content;
+
+}
+
